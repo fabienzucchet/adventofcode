96aeb47e7185897312efd75fc7b9872067f8dbc9
Fabien Zucchet <fabienz@aleios.com>
2024-11-26 17:09:04 +0000

    --wip-- [skip ci]

--- y2023/d05/fabienz/solution.go
+++ y2023/d05/fabienz/solution.go
@@ -3,6 +3,7 @@ package fabienz
 import (
 	"fmt"
 	"io"
+	"regexp"
 	"strconv"
 	"strings"
 
@@ -17,18 +18,19 @@ func PartOne(input io.Reader, answer io.Writer) error {
 		return fmt.Errorf("could not read input: %w", err)
 	}
 
-	a, err := parseInput(lines)
+	a, err := parseLines(lines)
 	if err != nil {
-		return fmt.Errorf("could not parse input: %w", err)
+		return fmt.Errorf("could not parse lines: %w", err)
 	}
 
-	locations := []int{}
-
-	for _, seed := range a.seeds {
-		locations = append(locations, a.getLocationForSeed(seed))
+	transformedSeeds, err := a.transformSeeds()
+	if err != nil {
+		return fmt.Errorf("could not transform seeds: %w", err)
 	}
 
-	_, err = fmt.Fprintf(answer, "%d", helpers.MinInts(locations))
+	lowestSeed := getLowestSeed(transformedSeeds)
+
+	_, err = fmt.Fprintf(answer, "%d", lowestSeed)
 	if err != nil {
 		return fmt.Errorf("could not write answer: %w", err)
 	}
@@ -44,9 +46,6 @@ func PartTwo(input io.Reader, answer io.Writer) error {
 		return fmt.Errorf("could not read input: %w", err)
 	}
 
-	// TODO: Write code to solve Part 2 here.
-
-	// TODO: Write your solution to Part 2 below.
 	_, err = fmt.Fprintf(answer, "%d", len(lines))
 	if err != nil {
 		return fmt.Errorf("could not write answer: %w", err)
@@ -55,149 +54,224 @@ func PartTwo(input io.Reader, answer io.Writer) error {
 	return nil
 }
 
-var LINES = map[string][2]int{
-	"SEED_TO_SOIL":            {3, 50},
-	"SOIL_TO_FERTILIZER":      {53, 88},
-	"FERTILIZER_TO_WATER":     {91, 120},
-	"WATER_TO_LIGHT":          {123, 148},
-	"LIGHT_TO_TEMPERATURE":    {151, 194},
-	"TEMPERATURE_TO_HUMIDITY": {197, 203},
-	"HUMIDITY_TO_LOCATION":    {206, 247},
+// 50 98 2 translates into MappedInterval{98, 2, 50-98}
+type MappedInterval struct {
+	Start  int
+	Length int
+	Offset int
 }
 
-// var LINES = map[string][2]int{
-// 	"SEED_TO_SOIL":            {3, 4},
-// 	"SOIL_TO_FERTILIZER":      {7, 9},
-// 	"FERTILIZER_TO_WATER":     {12, 15},
-// 	"WATER_TO_LIGHT":          {18, 19},
-// 	"LIGHT_TO_TEMPERATURE":    {22, 24},
-// 	"TEMPERATURE_TO_HUMIDITY": {27, 28},
-// 	"HUMIDITY_TO_LOCATION":    {31, 32},
-// }
+func (m *MappedInterval) String() string {
+	return fmt.Sprintf("[%d,%d] -> [%d, %d]", m.Start, m.Start+m.Length-1, m.Start+m.Offset, m.Start+m.Length-1+m.Offset)
+}
 
-type Range struct {
-	sourceStart      int
-	destinationStart int
-	length           int
+func (m *MappedInterval) Copy() MappedInterval {
+	return MappedInterval{m.Start, m.Length, m.Offset}
+}
+
+func (m *MappedInterval) Contains(value int) bool {
+	return value >= m.Start && value < m.Start+m.Length
+}
+
+func (m *MappedInterval) Map(value int) (int, error) {
+	if !m.Contains(value) {
+		return 0, fmt.Errorf("value %d is not in interval %v", value, m)
+	}
+	return value + m.Offset, nil
+}
+
+func parseInterval(s string) (MappedInterval, error) {
+	var targetStart, sourceStart, length int
+	_, err := fmt.Sscanf(s, "%d %d %d", &targetStart, &sourceStart, &length)
+	if err != nil {
+		return MappedInterval{}, fmt.Errorf("could not parse interval %q: %w", s, err)
+	}
+	return MappedInterval{sourceStart, length, targetStart - sourceStart}, nil
+}
+
+type MappedIntervals []MappedInterval
+
+func (mm MappedIntervals) String() string {
+	var s string
+	for _, m := range mm {
+		s += m.String() + " "
+	}
+	return s
+}
+
+// Given a list of intervals, sort them by their start value in ascending order
+func (mm MappedIntervals) Sort() {
+	for i := 0; i < len(mm); i++ {
+		for j := i + 1; j < len(mm); j++ {
+			if mm[i].Start > mm[j].Start {
+				mm[i], mm[j] = mm[j], mm[i]
+			}
+		}
+	}
+}
+
+func (mm MappedIntervals) Map(value int) (int, error) {
+	for _, m := range mm {
+		if m.Contains(value) {
+			mappedValue, err := m.Map(value)
+			if err != nil {
+				return 0, fmt.Errorf("could not map value %d: %w", value, err)
+			}
+			return mappedValue, nil
+		}
+	}
+	return value, nil
+}
+
+func parseSeeds(s string) (seeds MappedIntervals, err error) {
+
+	re := regexp.MustCompile(`\d+`)
+	rawSeeds := re.FindAllString(s, -1)
+
+	seeds = make([]MappedInterval, len(rawSeeds))
+
+	for i, rawSeed := range rawSeeds {
+		seed, err := strconv.Atoi(rawSeed)
+		if err != nil {
+			return nil, fmt.Errorf("could not parse seed %q: %w", seed, err)
+		}
+
+		seeds[i] = MappedInterval{seed, 1, 0}
+	}
+
+	// Sort the seeds by their start value
+	seeds.Sort()
+
+	return seeds, nil
 }
 
 type Almanac struct {
-	seeds                 []int
-	seedToSoil            []Range
-	soilToFertilizer      []Range
-	fertilizerToWater     []Range
-	waterToLight          []Range
-	lightToTemperature    []Range
-	temperatureToHumidity []Range
-	humidityToLocation    []Range
+	Seeds            MappedIntervals
+	AlmanacIntervals []MappedIntervals
 }
 
-func parseInput(lines []string) (a Almanac, err error) {
-	a = Almanac{}
+func (a *Almanac) transformSeed(seed int) (int, error) {
+	for _, interval := range a.AlmanacIntervals {
+		mappedSeed, err := interval.Map(seed)
+		if err != nil {
+			return 0, fmt.Errorf("could not map seed %d: %w", seed, err)
+		}
+		seed = mappedSeed
+	}
+	return seed, nil
+}
 
-	// Parse the seeds.
-	seeds := strings.Split(lines[0], " ")[1:]
+func (a *Almanac) transformSeeds() ([]int, error) {
+	transformedSeeds := make([]int, len(a.Seeds))
+	for i, seed := range a.Seeds {
+		transformedSeed, err := a.transformSeed(seed.Start)
+		if err != nil {
+			return nil, fmt.Errorf("could not transform seed %d: %w", seed, err)
+		}
+		transformedSeeds[i] = transformedSeed
+	}
+	return transformedSeeds, nil
+}
+
+// Merge two MappedIntervals using the following logic:
+// - We assume that both MappedIntervals are sorted by their start value in ascending order
+// - We follow a traditional merge algorithm but we need to keep the values of Offset distincts
+// - Any interval that is not overlapping will be added to the result with an Offset of 0
+func mergeIntervals(a, b MappedIntervals) MappedIntervals {
+	merged := MappedIntervals{}
+	i, j := 0, 0
+	for i < len(a) && j < len(b) {
+		// Case 1: a[i] is before b[j] - keep a[i] the same as we merge with an offset of 0
+		if a[i].Start+a[i].Length < b[j].Start {
+			merged = append(merged, a[i])
+			i++
+		} else if a[i].Start > b[j].Start {
+			merged = append(merged, b[j])
+			j++
+		} else {
+			// We have an overlap
+			// We need to keep the Offset distinct
+			if a[i].Offset < b[j].Offset {
+				merged = append(merged, a[i])
+			} else {
+				merged = append(merged, b[j])
+			}
+			i++
+			j++
+		}
+	}
+
+	// Add the remaining intervals
+	for i < len(a) {
+		merged = append(merged, a[i])
+		i++
+	}
+	for j < len(b) {
+		merged = append(merged, b[j])
+		j++
+	}
+
+	return merged
+}
+
+// We use the fact that the lowest seed will always be at the start of an interval
+func (a *Almanac) getLowestLocation() int {
+	lowestLocation := a.Seeds[0].Start
+	for _, interval := range a.AlmanacIntervals {
+		if interval[0].Start < lowestLocation {
+			lowestLocation = interval[0].Start
+		}
+	}
+	return lowestLocation
+}
+
+func getLowestSeed(seeds []int) int {
+	lowestSeed := seeds[0]
 	for _, seed := range seeds {
-		seedInt, err := strconv.Atoi(seed)
-		if err != nil {
-			return a, fmt.Errorf("could not parse seed: %w", err)
+		if seed < lowestSeed {
+			lowestSeed = seed
 		}
-		a.seeds = append(a.seeds, seedInt)
+	}
+	return lowestSeed
+}
+
+func parseLines(lines []string) (Almanac, error) {
+
+	a := Almanac{}
+	mapIdx := -1
+
+	for i, line := range lines {
+		if i == 0 {
+			seeds, err := parseSeeds(line)
+			if err != nil {
+				return Almanac{}, fmt.Errorf("could not parse seeds: %w", err)
+			}
+
+			a.Seeds = seeds
+			continue
+		}
+
+		if strings.Contains(line, "map:") {
+			mapIdx += 1
+			a.AlmanacIntervals = append(a.AlmanacIntervals, MappedIntervals{})
+			continue
+		}
+
+		if line != "" {
+			interval, err := parseInterval(line)
+			if err != nil {
+				return Almanac{}, fmt.Errorf("could not parse interval: %w", err)
+			}
+
+			a.AlmanacIntervals[mapIdx] = append(a.AlmanacIntervals[mapIdx], interval)
+			continue
+		}
 	}
 
-	// Parse the seed to soil map.
-	for i := LINES["SEED_TO_SOIL"][0]; i <= LINES["SEED_TO_SOIL"][1]; i++ {
-		r, err := parseRange(lines[i])
-		if err != nil {
-			return a, fmt.Errorf("could not parse seed to soil map: %w", err)
-		}
-		a.seedToSoil = append(a.seedToSoil, r)
-	}
-
-	// Parse the soil to fertilizer map.
-	for i := LINES["SOIL_TO_FERTILIZER"][0]; i <= LINES["SOIL_TO_FERTILIZER"][1]; i++ {
-		r, err := parseRange(lines[i])
-		if err != nil {
-			return a, fmt.Errorf("could not parse soil to fertilizer map: %w", err)
-		}
-		a.soilToFertilizer = append(a.soilToFertilizer, r)
-	}
-
-	// Parse the fertilizer to water map.
-	for i := LINES["FERTILIZER_TO_WATER"][0]; i <= LINES["FERTILIZER_TO_WATER"][1]; i++ {
-		r, err := parseRange(lines[i])
-		if err != nil {
-			return a, fmt.Errorf("could not parse fertilizer to water map: %w", err)
-		}
-		a.fertilizerToWater = append(a.fertilizerToWater, r)
-	}
-
-	// Parse the water to light map.
-	for i := LINES["WATER_TO_LIGHT"][0]; i <= LINES["WATER_TO_LIGHT"][1]; i++ {
-		r, err := parseRange(lines[i])
-		if err != nil {
-			return a, fmt.Errorf("could not parse water to light map: %w", err)
-		}
-		a.waterToLight = append(a.waterToLight, r)
-	}
-
-	// Parse the light to temperature map.
-	for i := LINES["LIGHT_TO_TEMPERATURE"][0]; i <= LINES["LIGHT_TO_TEMPERATURE"][1]; i++ {
-		r, err := parseRange(lines[i])
-		if err != nil {
-			return a, fmt.Errorf("could not parse light to temperature map: %w", err)
-		}
-		a.lightToTemperature = append(a.lightToTemperature, r)
-	}
-
-	// Parse the temperature to humidity map.
-	for i := LINES["TEMPERATURE_TO_HUMIDITY"][0]; i <= LINES["TEMPERATURE_TO_HUMIDITY"][1]; i++ {
-		r, err := parseRange(lines[i])
-		if err != nil {
-			return a, fmt.Errorf("could not parse temperature to humidity map: %w", err)
-		}
-		a.temperatureToHumidity = append(a.temperatureToHumidity, r)
-	}
-
-	// Parse the humidity to location map.
-	for i := LINES["HUMIDITY_TO_LOCATION"][0]; i <= LINES["HUMIDITY_TO_LOCATION"][1]; i++ {
-		r, err := parseRange(lines[i])
-		if err != nil {
-			return a, fmt.Errorf("could not parse humidity to location map: %w", err)
-		}
-		a.humidityToLocation = append(a.humidityToLocation, r)
+	// Sort the intervals by their start value
+	for i := range a.AlmanacIntervals {
+		a.AlmanacIntervals[i].Sort()
 	}
 
 	return a, nil
-}
-
-func parseRange(s string) (r Range, err error) {
-	fmt.Sscanf(s, "%d %d %d", &r.destinationStart, &r.sourceStart, &r.length)
-
-	return r, nil
-}
-
-func (a *Almanac) getLocationForSeed(seed int) (location int) {
-	soil := findInMap(a.seedToSoil, seed)
-	fertilizer := findInMap(a.soilToFertilizer, soil)
-	water := findInMap(a.fertilizerToWater, fertilizer)
-	light := findInMap(a.waterToLight, water)
-	temperature := findInMap(a.lightToTemperature, light)
-	humidity := findInMap(a.temperatureToHumidity, temperature)
-	location = findInMap(a.humidityToLocation, humidity)
-
-	return location
-}
-
-func findInMap(m []Range, idx int) (newIdx int) {
-	newIdx = idx
-
-	for _, r := range m {
-		if idx >= r.sourceStart && idx < r.sourceStart+r.length {
-			newIdx = r.destinationStart + (idx - r.sourceStart)
-			break
-		}
-	}
-
-	return newIdx
 }
